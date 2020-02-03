cdb3f85e70cbb312e0f4b56b5b056a53057786fc
Spencer <wf6DJd8a3xSSCZbn@protonmail.com>
2020-02-03 16:09:43 -0500

    basic login template added

--- main.go
+++ main.go
@@ -3,11 +3,23 @@ package main
 import (
 	"log"
 	"net/http"
+	"html/template"
 )
+
+type LoginPage struct {
+	Title string
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	p := LoginPage{Title: "Login"}
+	t, _ := template.ParseFiles("templates/login.html")
+	t.Execute(w, p)
+}
 
 func main() {
 	port := "8080"
 	http.Handle("/", http.FileServer(http.Dir("static/")))
+	http.HandleFunc("/login", loginHandler)
 
 	log.Println("Server started on port " + port)
 
