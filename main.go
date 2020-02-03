4210f4edf97594378ba3a0d389b8cb314b157f56
Spencer <wf6DJd8a3xSSCZbn@protonmail.com>
2020-02-03 18:52:12 -0500

    added db connection

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"html/template"
+	"./env"
+	"database/sql"
+	_ "github.com/lib/pq"
 )
 
 type LoginPage struct {
@@ -20,11 +24,16 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 		t, _ := template.ParseFiles("templates/login.html")
 		t.Execute(w, p)
 	case "POST":
-		log.Println("Post request!")
+		log.Println(r.FormValue("username"))
+		log.Println(r.FormValue("password"))
 	}
 }
 
 func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s " + "password=%s dbname=%s sslmode=disable", 
+	env.Host, env.Port, env.User, env.Passwd, env.Dbname)
+	db, _ := sql.Open("postgres", psqlInfo)
+	defer db.Close()
 	port := "8080"
 	http.Handle("/", http.FileServer(http.Dir("static/")))
 	http.HandleFunc("/login", loginHandler)
