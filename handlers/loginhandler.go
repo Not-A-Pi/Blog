de54b0e3deb26e2288fb9e43bb9ca0e164a12fa2
Spencer <wf6DJd8a3xSSCZbn@protonmail.com>
2020-02-10 23:48:43 -0500

    added user auth

--- handlers/loginhandler.go
+++ handlers/loginhandler.go
@@ -5,6 +5,12 @@ import (
 	"html/template"
 	"log"
 	"../structs/Loginpage"
+	"database/sql"
+	"github.com/joho/godotenv"
+	"fmt"
+	"os"
+	"golang.org/x/crypto/bcrypt"
+	_ "github.com/lib/pq"
 )
 
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
@@ -14,7 +20,18 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 		t, _ := template.ParseFiles("templates/login.html")
 		t.Execute(w, p)
 	case "POST":
-		log.Println(r.FormValue("username"))
-		log.Println(r.FormValue("password"))
+		godotenv.Load("../.env")
+		psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("HOST"), os.Getenv("PORT"), os.Getenv("DBUSER"), os.Getenv("PASSWD"), os.Getenv("DBNAME"))
+		db, _ := sql.Open("postgres", psqlInfo)
+		defer db.Close()
+		var passhash string
+		_ = db.QueryRow("select password from users where username=$1", r.FormValue("username")).Scan(&passhash)
+		err := bcrypt.CompareHashAndPassword([]byte(passhash), []byte(r.FormValue("password")))
+		if err == nil {
+			log.Println("Logged in")
+		} else {
+			log.Println("Not logged in")
+		}
 	}
 }
